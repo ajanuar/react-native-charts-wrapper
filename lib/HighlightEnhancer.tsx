2ebe8eb107b8087ecec9b5852843f84b3857cd3e
Albert Januar <albetjanuar@gmail.com>
2025-03-09 20:46:56 +0700

    Move to native

--- lib/HighlightEnhancer.tsx
+++
@@ -1,9 +0,0 @@
-export default function HighlightEnhancer(Chart) {
-  return class HighlightExtended extends Chart {
-    highlights(config) {
-      if (this.getNativeComponentRef()?.current) {
-        this.getNativeComponentRef().current.highlights(config);
-      }
-    }
-  }
-}
--- lib/ScaleEnhancer.tsx
+++
@@ -1,9 +0,0 @@
-export default function ScaleEnhancer(Chart) {
-  return class ScaleExtended extends Chart {
-    fitScreen() {
-      if (this.getNativeComponentRef()?.current) {
-        this.getNativeComponentRef().current.fitScreen();
-      }
-    }
-  }
-}
