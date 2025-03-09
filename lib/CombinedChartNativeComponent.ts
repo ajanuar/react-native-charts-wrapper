9a183073965193c200f97bd04b65bdb7a0ec37c5
Albert Januar <albetjanuar@gmail.com>
2025-03-09 20:34:28 +0700

    Delete CombinedChart

---
+++ lib/CombinedChartNativeComponent.ts
@@ -0,0 +1,55 @@
+import type { HostComponent, ViewProps } from 'react-native';
+import type { BubblingEventHandler } from 'react-native/Libraries/Types/CodegenTypes';
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+import codegenNativeCommands from 'react-native/Libraries/Utilities/codegenNativeCommands';
+import { BarLineChartBaseProps } from './BarLineChartBase';
+import { CombinedData } from './ChartDataConfig';
+
+export interface NativeProps extends ViewProps, BarLineChartBaseProps {
+  data?: CombinedData;
+  drawOrder?: Array<'BAR' | 'BUBBLE' | 'LINE' | 'CANDLE' | 'SCATTER'>;
+  drawValueAboveBar?: boolean;
+  highlightFullBarEnabled?: boolean;
+  drawBarShadow?: boolean;
+  onChange?: BubblingEventHandler<void>;
+  onSelect?: BubblingEventHandler<void>;
+}
+
+export type ComponentType = HostComponent<NativeProps>;
+
+// Combined native commands from all enhancers
+interface NativeCommands {
+  // Move enhancer commands
+  moveViewTo: (viewRef: React.ComponentRef<ComponentType>, x: number, y: number, axisDependency: string) => void;
+  moveViewToX: (viewRef: React.ComponentRef<ComponentType>, x: number) => void;
+  moveViewToAnimated: (viewRef: React.ComponentRef<ComponentType>, x: number, y: number, axisDependency: string, duration: number) => void;
+  centerViewTo: (viewRef: React.ComponentRef<ComponentType>, x: number, y: number, axisDependency: string) => void;
+  centerViewToAnimated: (viewRef: React.ComponentRef<ComponentType>, x: number, y: number, axisDependency: string, duration: number) => void;
+  
+  // Scale enhancer commands
+  fitScreen: (viewRef: React.ComponentRef<ComponentType>) => void;
+  
+  // Highlight enhancer commands
+  highlights: (viewRef: React.ComponentRef<ComponentType>, config: any[]) => void;
+  
+  // Scroll enhancer commands
+  setDataAndLockIndex: (viewRef: React.ComponentRef<ComponentType>, data: CombinedData) => void;
+}
+
+// @ts-ignore -- migration to react-native 0.73+
+export const Commands: NativeCommands = codegenNativeCommands<NativeCommands>({
+  supportedCommands: [
+    'moveViewTo',
+    'moveViewToX',
+    'moveViewToAnimated',
+    'centerViewTo',
+    'centerViewToAnimated',
+    'fitScreen',
+    'highlights',
+    'setDataAndLockIndex'
+  ],
+});
+
+export default codegenNativeComponent<NativeProps>(
+  'RNCombinedChart'
+) as HostComponent<NativeProps>;
